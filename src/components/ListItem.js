8d1aa2abf86aeadec8dd51ce64608737e8e7e445
Ivan <Ivan@MacBook-Pro-de-Ivan.local>
2017-04-26 10:38:13 -0300

    Added listItem  and selectReducer

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+    renderDescription() {
+        const { library, selectedLibraryID } = this.props;
+        // console.log(`library: ${library.id}, selectedLibraryID: ${selectedLibraryID}`);
+        if (library.id === selectedLibraryID) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
-
-        return(
-            <CardSection>
-                <Text style={titleStyle}>     
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+        const { id, title } = this.props.library;
+        return (
+            <TouchableWithoutFeedback 
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>     
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
-};
+}
 
 const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
     }
-}
+};
 
-export default ListItem;
+const MapStateToProps = state => {
+    return { selectedLibraryID: state.selectedLibraryID };
+};
+export default connect(MapStateToProps, actions)(ListItem);
+
+// first parameter passes the props and the second passes the actions. 
